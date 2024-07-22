1cafa7a683a4a659a3a0faf097f214c7a23eec40
QUAYEMX1 <126396459+QUAYEMX1@users.noreply.github.com>
2024-07-22 15:13:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,26 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+
+	let res=[]
+	let arr=[]
+	let sum=0;
+
+	for(let i=0;i<arr.length;i++){
+		if(sum+arr[i]<=n){
+			sum+=arr[i]
+			arr.push(arr[i])
+		}else{
+			res.push(arr)
+			arr=[arr[i]]
+			sum=arr[i]
+		}
+	}
+
+	res.push(arr)
+
+	return res
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+// const n = prompt("Enter n: ");
+// alert(JSON.stringify(divide(arr, n)));
