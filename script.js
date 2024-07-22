bd3fda399e5b6a278aee745bb1c11f32ff86810b
QUAYEMX1 <126396459+QUAYEMX1@users.noreply.github.com>
2024-07-22 15:26:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,24 +4,24 @@ const divide = (arr, n) => {
   // Write your code here
 
 	let res=[]
-	let arr=[]
+	let arr1=[]
 	let sum=0;
 
 	for(let i=0;i<arr.length;i++){
 		if(sum+arr[i]<=n){
 			sum+=arr[i]
-			arr.push(arr[i])
+			arr1.push(arr[i])
 		}else{
-			res.push(arr)
-			arr=[arr[i]]
+			res.push(arr1)
+			arr1=[arr[i]]
 			sum=arr[i]
 		}
 	}
 
-	res.push(arr)
+	res.push(arr1)
 
 	return res
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+// const n = prompt("Enter n: ");
+// alert(JSON.stringify(divide(arr, n)));
